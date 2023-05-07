94adfb4d8839ba3858ee6f4dffad3df0280c1455
Ryzal Fadhillah <fadhillah.ryzal@gmail.com>
2023-05-08 01:01:50 +0700

    [Create] Navbar

---
+++ zoonami/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,37 @@
+import { Link } from 'react-router-dom';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import logo from '../../assets/images/logo.png';
+
+function Navigation() {
+    return (
+        <Navbar expand="lg" style={{ backgroundColor: '#3C6255' }}>
+        <Container>
+            <Navbar.Brand className="mr-auto">
+            <Link to="/">
+                <img
+                src={logo}
+                width="30"
+                height="30"
+                className="d-inline-block align-top"
+                alt="Logo"
+                />
+            </Link>
+            <span className="ml-2 text-white">zoonami</span>
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="navbar" />
+            <Navbar.Collapse id="navbar" className="justify-content-end">
+            <Nav>
+                <Nav.Item>
+                <Link to="/" className="nav-link text-white">Home</Link>
+                </Nav.Item>
+                <Nav.Item>
+                <Link to="/animals" className="nav-link text-white">Animals</Link>
+                </Nav.Item>
+            </Nav>
+            </Navbar.Collapse>
+        </Container>
+        </Navbar>
+    );
+}
+
+export default Navigation;
