d9bfa7b430019f03ef79ea0b1e07e185f349664a
Ryzal Fadhillah <fadhillah.ryzal@gmail.com>
2023-05-09 20:52:35 +0700

    [Create] Ticket

--- zoonami/src/pages/LandingPage/LandingPage.jsx
+++ zoonami/src/pages/LandingPage/LandingPage.jsx
@@ -5,14 +5,16 @@ import HeroImage from "../../components/HeroSection/Hero"
 import Navigation from "../../components/Navbar/Navbar"
 import Footer from '../../components/Footer/Footer'
 import Facilities from '../../components/Fasilitas/Fasilitas'
+import Ticket from '../../components/Tiket/Tiket'
 
 const LandingPage = () => {
     return (
         <div style={{ backgroundColor: '#61876E' }}>
             <Navigation />
             <HeroImage />
-                <About />
+            <About />
             <Facilities />
+            <Ticket />
             <Footer />
         </div>
     )
