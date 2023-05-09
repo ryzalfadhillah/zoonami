fa551169180da540b09d97f2ecaa3bebb3ea327f
Ryzal Fadhillah <fadhillah.ryzal@gmail.com>
2023-05-09 19:39:56 +0700

    [Create] Footer

--- zoonami/src/pages/LandingPage/LandingPage.jsx
+++ zoonami/src/pages/LandingPage/LandingPage.jsx
@@ -3,15 +3,17 @@ import './LandingPage.css'
 import About from "../../components/AboutUs/aboutUs"
 import HeroImage from "../../components/HeroSection/Hero"
 import Navigation from "../../components/Navbar/Navbar"
+import Footer from '../../components/Footer/Footer'
 
 const LandingPage = () => {
     return (
         <div style={{ backgroundColor: '#61876E' }}>
             <Navigation />
             <HeroImage />
-            <div className="mt-4">
+            <div className="my-4">
                 <About />
             </div>
+            <Footer />
         </div>
     )
 }
